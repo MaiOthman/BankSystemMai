92e579632e8c861da6b7ef7dc204e1947ae56a5c
MaiOthman <mai.m.othman94@gmail.com>
2025-02-13 10:53:30 +0100

    Initial Commit

---
+++ BankSystemFirst/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BankSystemFirst
+//
+//  Created by Mai Mahmoud on 13.02.25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
